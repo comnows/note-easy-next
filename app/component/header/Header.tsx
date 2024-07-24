cf32c110be59b5b475141da1596314ad171e1b06
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-07-24 17:13:08 +0700

    feat: show username in header

--- app/component/header/Header.tsx
+++ app/component/header/Header.tsx
@@ -1,7 +1,9 @@
+import { useAuthContext } from "@/app/hook/UseAuthContext";
 import { auth } from "@/app/lib/firebase";
 import { signOut } from "firebase/auth";
 
 function Header() {
+  const { currentUser } = useAuthContext();
   const handleLogout = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
@@ -11,7 +13,7 @@ function Header() {
 
   return (
     <header className="flex justify-between gap-4 bg-white rounded-full px-20 py-3">
-      <p className="text-lg font-semibold">John Doe</p>
+      <p className="text-lg font-semibold">{currentUser?.displayName}</p>
       <button onClick={handleLogout} className="text-red-600 text-base">
         Logout
       </button>
